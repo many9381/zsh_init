17864d69a972ab3e838a9d359e4eb1636c0db3ed
many.homete <many.homete@gmail.com>
2021-01-02 20:34:09 +0900

    Add install zsh, copy zshrc

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+# install zsh
+apt install zsh -y
+
 # installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -16,12 +19,12 @@ git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.oh-my-zsh/c
 # install zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-
-
-
-
 # install powerlevel10k
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+
+# copy zshrc to local zshrc
+cp ./zshrc ~/.zshrc
 
 
 # apply .zshrc
