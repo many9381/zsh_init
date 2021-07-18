d19841e75ec42975114dfd37fb5a7921daa4f7db
many9381 <many9381@gmail.com>
2021-07-19 00:53:18 +0900

    ADD: Add tmux, tmuxinator, oh-my-tmux install

--- tools/install.sh
+++ tools/install.sh
@@ -5,57 +5,80 @@
 #  exit
 #fi
 
-# install zsh
+# Install zsh
+# ------------------------------------------------------------
 sudo apt update
 sudo apt install zsh git curl thefuck build-essential -y
 
-# install lazygit
+# Install lazygit
+# ------------------------------------------------------------
 sudo add-apt-repository ppa:lazygit-team/release
 sudo apt-get update
 sudo apt-get install lazygit
 
-# install bat
+# Install bat
+# ------------------------------------------------------------
 sudo apt install bat
 
 # Add bat link
+# ------------------------------------------------------------
 sudo ln -s /usr/bin/batcat /usr/bin/bat
 
+
+# Install tmux, tmuxinator, oh-my-tmux
+# ------------------------------------------------------------
+sudo apt install tmux tmuxinator
+
+git clone https://github.com/gpakosz/.tmux.git ~
+ln -sf ~/.tmux/.tmux.conf ~/.tmux.conf
+cp ~/.tmux/.tmux.conf.local ~
+
 # Set ZSH, ZSH_CUSTOM
+# ------------------------------------------------------------
 ZSH="${HOME}/.oh-my-zsh"
 ZSH_CUSTOM="$ZSH/custom"
 
-# installing oh-my-zsh
+# Installing oh-my-zsh
+# ------------------------------------------------------------
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # install oh-my-zsh plugins
 
-# install fast-syntax-highlighting
+# Install fast-syntax-highlighting
+# ------------------------------------------------------------
 git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 
-# install zsh-autosuggestions
+# Install zsh-autosuggestions
+# ------------------------------------------------------------
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-# install powerlevel10k
+# Install powerlevel10k
+# ------------------------------------------------------------
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-# install zsh-256color
+# Install zsh-256color
+# ------------------------------------------------------------
 git clone https://github.com/chrissicool/zsh-256color $ZSH_CUSTOM/plugins/zsh-256color
 
 # copy zshrc to local zshrc
+# ------------------------------------------------------------
 cp ../zshrc ~/.zshrc
 
 # Install fzf
+# ------------------------------------------------------------
 echo "Install fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # change default shell
+# ------------------------------------------------------------
 echo "Change default shell to zsh"
 
 zsh=$(which zsh)
 chsh -s "${zsh}"
 
 # Install lsd
+# ------------------------------------------------------------
 echo "Install lsd"
 wget https://github.com/Peltoche/lsd/releases/download/0.19.0/lsd_0.19.0_amd64.deb
 sudo dpkg -i lsd_0.19.0_amd64.deb
@@ -64,9 +87,11 @@ echo "Remove lsd install file"
 rm lsd_0.19.0_amd64.deb
 
 # Install zplug
+# ------------------------------------------------------------
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # Install zplugins
+# ------------------------------------------------------------
 zplug install | zsh
 
 zsh
